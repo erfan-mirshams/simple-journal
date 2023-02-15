5315ea7a51dc419ec34be404b0546a4b1892c489
Erfan Mirshams <erfanmirshams@gmail.com>
2023-02-15 23:55:10 +0330

    implement first_word function

--- code.cpp
+++ code.cpp
@@ -1,8 +1,37 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cctype>
+
+#define COMMANDSIZE 4
+typedef struct date date;
 
 using namespace std;
 
+struct date {
+    int year;
+    int month;
+    int day;
+};
+
+string first_word_from_pos(string::iterator &it, string &line){
+    while(isspace(*(it++)));
+    --it;
+    string::iterator i = it;
+    for (; i != line.end(); ++i) {
+        if(isspace(*i)){
+            break;
+        }
+    }
+    swap(i, it);
+    return line.substr(i - line.begin(), it - i);
+}
+
 int main(){
+    string line = "", current_journal = "";
+    string commands[COMMANDSIZE] = {"start_day", "show_day", "show_the_longest_day", "show_the_best_day"};
+    while (cin.peek() != EOF) {
+        getline(cin, line);
+    }
     return 0;
 }
